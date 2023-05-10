302d869369bdc52b515d5abee8b9e82130e270cd
sabbir <khandokarsabbir@gmail.com>
2023-05-09 19:30:54 -0500

    Sql for Tables and Functions

---
+++ Solution Items/TablesAndFunctions.sql
@@ -0,0 +1,64 @@
+
+-- ParkIn Table
+CREATE TABLE [dbo].[ParkIn](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[TagNumber] [varchar](10) NOT NULL,
+	[CheckIn] [datetime] NOT NULL,
+ CONSTRAINT [PK_ParkIn] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [TagNumberUnique] UNIQUE NONCLUSTERED 
+(
+	[TagNumber] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+-- ParkOut Table
+CREATE TABLE [dbo].[ParkOut](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[InsertTime] [datetime] NOT NULL,
+	[TagNumber] [varchar](10) NOT NULL,
+	[CheckIn] [datetime] NOT NULL,
+	[CheckOut] [datetime] NOT NULL,
+	[ElaspedTime] [decimal](18, 2) NOT NULL,
+	[HourlyFee] [decimal](18, 2) NOT NULL,
+	[Total] [decimal](18, 2) NOT NULL,
+ CONSTRAINT [PK_ParkOut] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+-- TotalHours function
+CREATE FUNCTION [dbo].[TotalHours](
+    @fromDate DateTime,
+    @todate DateTime
+)
+RETURNS decimal(10,2)
+AS 
+BEGIN
+    
+	DECLARE @hours AS decimal(10,2);
+
+	IF(DATEDIFF(MINUTE, @fromDate, @Todate) <= 0)
+	BEGIN
+	   SET @hours = 0; 
+	END
+	ELSE IF(DATEDIFF(MINUTE, @fromDate, @Todate) >= 60)
+	BEGIN
+	  -- SET @hours = round(CONCAT(DATEDIFF(MINUTE, @fromDate, @todate)/60, '.', DATEDIFF(MINUTE, @fromDate, @todate)%60),2);
+	  -- SET @hours = round(DATEDIFF(MINUTE, @fromDate, @todate)/60.00,2); 
+	  SET @hours = round(DATEDIFF(MINUTE, @fromDate, @todate)/60 + DATEDIFF(MINUTE, @fromDate, @todate)%60/100.0,2);
+	END
+	ELSE
+	BEGIN
+	   SET @hours = round(DATEDIFF(MINUTE, @fromDate, @todate)/100.0,2);
+	END
+
+    RETURN @hours;
+END;
+
